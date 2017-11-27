90fe235505410dc2d79671e485d13b0569376e53
AsgeDarr <apdarr@gmail.com>
2017-11-27 15:57:35 +0100

    start

--- MicroCProject/antlr.sh
+++
@@ -1,5 +0,0 @@
-java -jar lib/antlr-4.7-complete.jar src/MicroC_language/parsing/MicroC.g4 
-javac -sourcepath src -cp lib/*.jar src/MicroC_language/MicroC.java src/MicroC_language/parsing/*.java -d bin
-cd bin
-java MicroC ../tests/test1.microC -cp ../lib/antlr-4.7-complete.jar 
-cd ..
