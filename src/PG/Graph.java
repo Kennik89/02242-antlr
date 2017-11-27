2a5285e4cfffe50da1504883f9994be2675da72f
Kennik89 <kand89@gmail.com>
2017-11-27 16:27:22 +0100

    Graph

--- src/PG/Graph.java
+++ src/PG/Graph.java
@@ -1,10 +1,13 @@
 package PG;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Graph {
 	
 	ArrayList<Node> nodes = new ArrayList<Node>();
 	ArrayList<Edge> edges = new ArrayList<Edge>();
+	Node initalNode;
+	LinkedList<Node> finalNodes = new LinkedList<Node>();
 
 }
