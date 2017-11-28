9e8f0a97e924d951a149871d6d22762ae12a82e3
Kennik89 <kand89@gmail.com>
2017-11-28 14:59:12 +0100

    ...

--- src/PG/Edge.java
+++ src/PG/Edge.java
@@ -2,15 +2,9 @@ package PG;
 
 class Edge {
 	
-<<<<<<< HEAD
-	public Node from;
-	public Node to;
-	public String label;
-=======
 	private Node from;
 	private Node to;
 	private String code;
->>>>>>> origin/master
 
 	public Edge(Node from, Node to, String code)	{
 		this.from = from;
