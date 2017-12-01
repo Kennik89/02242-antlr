469fc3d6751fff35fc637e99ef87f699a1e3065a
Kennik89 <kand89@gmail.com>
2017-12-01 18:32:58 +0100

    ...

--- src/PG/PG.java
+++ src/PG/PG.java
@@ -2,15 +2,41 @@ package PG;
 
 
 public class PG {
+	static Graph pg1, pg2, pg3;
 	
 	
 	public static void main(String args[] ) {
-		Graph pg = new Graph();
-	
-		Node q0 = pg.addInitialNode(); // q0
-		Node q1 = pg.addNode(); // q1
-		Node q2 = pg.addFinalNode(); // q2
+		test1();
+		test2();
+		test3();
+	}
+
+	private static void test3() {
+		pg1 = new Graph();
 		
-		pg.addEdge(q0, q1, "int x");
+		Node q0 = pg1.addInitialNode(); // q0
+		Node q1 = pg1.addNode(); // q1
+		Node q2 = pg1.addFinalNode(); // q2
+		
+		pg1.addEdge(q0, q1, "int x");
+	}
+
+	private static void test2() {
+		pg2 = new Graph();
+		
+		Node q0 = pg2.addInitialNode();
+		Node q1 = pg2.addNode();
+		Node q2 = pg2.addNode();
+		NOde
+	}
+
+	private static void test1() {
+		Graph pg1 = new Graph();
+	
+		Node q0 = pg1.addInitialNode(); // q0
+		Node q1 = pg1.addNode(); // q1
+		Node q2 = pg1.addFinalNode(); // q2
+		
+		pg1.addEdge(q0, q1, "int x");
 	}
 }
--- src/ReachingDef/Analysis.java
+++ src/ReachingDef/Analysis.java
@@ -2,6 +2,12 @@ package ReachingDef;
 
 public class Analysis {
 	
+	// Collect all variables
+	// Set all variables to {x,?}
+	// Set up the worklist
+	// Execute the worklist
+	// Print the result
 	
-
+	
+	
 }
